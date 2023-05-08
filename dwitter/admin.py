22311f716aa3d54401cfac250cc75f705e9ee366
Kenneth Shimabukuro <kenneth.r.shima@gmail.com>
2023-05-08 12:00:05 +0200

    created initial structure and setup models.

---
+++ dwitter/admin.py
@@ -0,0 +1,15 @@
+# dwitter/admin.py
+
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from .models import Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ["username"]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Profile)
