67a69cef775189048e14d644e2cefebf49e7cdf8
Kenneth Shimabukuro <kenneth.r.shima@gmail.com>
2023-05-10 10:55:58 +0200

    added show list of users if on profile page

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 def dashboard(request):
     return render(request, "base.html")
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "dwitter/profile_list.html", {"profiles": profiles})
